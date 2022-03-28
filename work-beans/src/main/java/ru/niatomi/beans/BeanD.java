6376ca7add7e77fd390ceb6477e04aa9bb9a3542
Niatomi <playervoker@gmail.com>
2022-03-29 00:36:02 +0400

    Add methods cycle show

--- work-beans/src/main/java/ru/niatomi/beans/BeanD.java
+++ work-beans/src/main/java/ru/niatomi/beans/BeanD.java
@@ -4,4 +4,13 @@ package ru.niatomi.beans;
  * @author niatomi
  */
 public class BeanD {
+    @PostConstruct
+    public void initMethod() {
+        System.out.println("Bean D init method");
+    }
+
+    @PreDestroy
+    public void destroyMethod() {
+        System.out.println("Bean D destroy method");
+    }
 }
--- work-beans/src/main/java/ru/niatomi/beans/BeanE.java
+++ work-beans/src/main/java/ru/niatomi/beans/BeanE.java
@@ -6,4 +6,13 @@ package ru.niatomi.beans;
 @Component
 @Scope("prototype")
 public class BeanE {
+    @PostConstruct
+    public void initMethod() {
+        System.out.println("Bean E init method");
+    }
+
+    @PreDestroy
+    public void destroyMethod() {
+        System.out.println("Bean E destroy method");
+    }
 }
