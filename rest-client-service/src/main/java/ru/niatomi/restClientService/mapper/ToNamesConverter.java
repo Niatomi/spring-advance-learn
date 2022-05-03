41405e703de2bb5373ffae86502e20e2b687cba1
Niatomi <playervoker@gmail.com>
2022-05-03 23:46:41 +0400

    Initial commit

---
+++ rest-client-service/src/main/java/ru/niatomi/restClientService/mapper/ToNamesConverter.java
@@ -0,0 +1,17 @@
+package ru.niatomi.restClientService.mapper;
+
+import org.mapstruct.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author niatomi
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.METHOD})
+@Qualifier
+public @interface ToNamesConverter {
+}
