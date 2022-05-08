aafb17331a633d9091ea9364fd2402e4f9a0d817
Niatomi <playervoker@gmail.com>
2022-05-09 00:59:18 +0400

    Initial commit

--- rest-client-service/src/main/java/ru/niatomi/restClientService/service/ClientService.java
+++ rest-client-service/src/main/java/ru/niatomi/restClientService/service/ClientService.java
@@ -1,10 +1,25 @@
 package ru.niatomi.restClientService.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import ru.niatomi.restClientService.model.domain.Client;
+import ru.niatomi.restClientService.model.dto.ClientDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 /**
  * @author niatomi
  */
-@Service
-public class ClientService {
+@Validated
+public interface ClientService {
+
+    String signUpClient(ClientDto clientDto);
+    String deleteClient(Long id);
+    String updateClient(Long id, ClientDto clientDto);
+
+    Client getClient(Long id);
+    Page<Client> getClients(Pageable page);
+    Page<Client> getClientsBySecondName(Pageable page);
+    Page<Client> getClientsByMail(Pageable page);
+
 }
