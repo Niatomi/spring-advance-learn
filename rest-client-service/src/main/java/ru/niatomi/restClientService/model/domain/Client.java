be3ecd85f94a4c7ee95754cba50142ff865f2f27
Niatomi <playervoker@gmail.com>
2022-05-09 15:08:14 +0400

    Add validation

--- rest-client-service/src/main/java/ru/niatomi/restClientService/model/domain/Client.java
+++ rest-client-service/src/main/java/ru/niatomi/restClientService/model/domain/Client.java
@@ -3,10 +3,14 @@ package ru.niatomi.restClientService.model.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
-import java.util.UUID;
 
 /**
  * @author niatomi
@@ -19,14 +23,17 @@ import java.util.UUID;
 public class Client {
 
     @Id
-    @Column(name = "id", length = 8)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_name", length = 32)
+    @Column(name = "first_name", length = 32, nullable = false)
+    @Pattern(regexp = "[а-яёА-ЯЁ-]+", message = "Должны быть только русские символы")
     private String firstName;
-    @Column(name = "second_name", length = 32)
+    @Column(name = "second_name", length = 32, nullable = false)
+    @Pattern(regexp = "[а-яёА-ЯЁ-]+", message = "Должны быть только русские символы")
     private String secondName;
-    @Column(name = "last_name", length = 32)
+    @Column(name = "last_name", length = 32, nullable = true)
+    @Pattern(regexp = "[а-яёА-ЯЁ-]+", message = "Должны быть только русские символы")
     private String thirdName;
 
     @Column(name = "gender")
@@ -34,15 +41,20 @@ public class Client {
     private Gender gender;
 
     @Column(name = "birthdate")
+    @DateTimeFormat(pattern = "dd-mm-yyyy")
     private LocalDate birthDate;
 
     @Column(name = "contact_number")
+    @Pattern(regexp = "^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$",
+             message = "Номер телефона должен быть похож на +79999999999, 8(999)999-99-99 , +7 999 999 99 99")
     private String contactNumber;
 
     @Column(name = "email")
+    @Email
     private String email;
 
     @Column(name = "sign_up_date")
+    @CreatedDate
     private LocalDate signUpDate;
 
     @Column(name = "delete_date")
