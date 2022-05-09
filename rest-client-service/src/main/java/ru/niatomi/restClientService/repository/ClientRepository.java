41c2530da0bc340c92a3377be46efc9cb2300106
Niatomi <playervoker@gmail.com>
2022-05-09 15:10:23 +0400

    Renamed and added Paging repo

--- rest-client-service/src/main/java/ru/niatomi/restClientService/repository/ClientRepository.java
+++ rest-client-service/src/main/java/ru/niatomi/restClientService/repository/ClientRepository.java
@@ -1,10 +1,11 @@
 package ru.niatomi.restClientService.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.niatomi.restClientService.model.domain.Client;
 
 /**
  * @author niatomi
  */
-public interface ClientViewRepository extends PagingAndSortingRepository<Client, Long> {
+public interface ClientRepository extends CrudRepository<Client, Long>, PagingAndSortingRepository<Client, Long> {
 }
