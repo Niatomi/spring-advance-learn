204f80f941af7c914a3b81869adc470fabe333cb
Niatomi <playervoker@gmail.com>
2022-05-12 01:57:29 +0400

    Deleted already extended via PagingAndSortingRepo CRUDrepo

--- rest-client-service/src/main/java/ru/niatomi/restClientService/repository/ClientRepository.java
+++ rest-client-service/src/main/java/ru/niatomi/restClientService/repository/ClientRepository.java
@@ -1,11 +1,10 @@
 package ru.niatomi.restClientService.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.niatomi.restClientService.model.domain.Client;
 
 /**
  * @author niatomi
  */
-public interface ClientRepository extends CrudRepository<Client, Long>, PagingAndSortingRepository<Client, Long> {
+public interface ClientRepository extends PagingAndSortingRepository<Client, Long> {
 }
