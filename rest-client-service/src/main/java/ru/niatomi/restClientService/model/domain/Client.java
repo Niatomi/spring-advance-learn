473864fdf12e1e94a7989bee0972a62dd94f8e9d
Niatomi <playervoker@gmail.com>
2022-05-12 00:17:17 +0400

    Column annotations deleted

--- rest-client-service/src/main/java/ru/niatomi/restClientService/model/domain/Client.java
+++ rest-client-service/src/main/java/ru/niatomi/restClientService/model/domain/Client.java
@@ -24,41 +24,32 @@ public class Client {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "first_name", length = 32, nullable = false)
     @Pattern(regexp = "[а-яёА-ЯЁ-]+", message = "Должны быть только русские символы")
     private String firstName;
 
-    @Column(name = "second_name", length = 32, nullable = false)
     @Pattern(regexp = "[а-яёА-ЯЁ-]+", message = "Должны быть только русские символы")
     private String secondName;
 
-    @Column(name = "last_name", length = 32)
     @Pattern(regexp = "[а-яёА-ЯЁ-]+", message = "Должны быть только русские символы")
     private String thirdName;
 
-    @Column(name = "gender")
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
-    @Column(name = "birthdate")
     @DateTimeFormat(pattern = "dd-mm-yyyy")
     @PastOrPresent(message = "Дата рождения не может быть в будующем")
     private LocalDate birthDate;
 
-    @Column(name = "contact_number", unique = true)
     @Pattern(regexp = "^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$",
              message = "Номер телефона должен быть похож на +79999999999, 8(999)999-99-99 , +7 999 999 99 99")
     private String contactNumber;
 
-    @Column(name = "email", unique = true)
     @Email(message = "Почта должна быть похожа на example@example.example")
     private String email;
 
-    @Column(name = "sign_up_date")
     @CreatedDate
     private LocalDate signUpDate;
 
-    @Column(name = "delete_date")
     private LocalDate deleteDate;
 
 }
