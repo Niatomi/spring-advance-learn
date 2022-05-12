8296ab8a562db0a8344af86c8725318319addc6a
Niatomi <playervoker@gmail.com>
2022-05-12 04:57:22 +0400

    Add methods for validation on service layer

--- rest-client-service/src/main/java/ru/niatomi/restClientService/repository/ClientRepository.java
+++ rest-client-service/src/main/java/ru/niatomi/restClientService/repository/ClientRepository.java
@@ -1,5 +1,7 @@
 package ru.niatomi.restClientService.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.niatomi.restClientService.model.domain.Client;
 
@@ -7,4 +9,7 @@ import ru.niatomi.restClientService.model.domain.Client;
  * @author niatomi
  */
 public interface ClientRepository extends PagingAndSortingRepository<Client, Long> {
+    Page<Client> findBySecondName(String secondName, Pageable page);
+    Client findByEmail(String email);
+    Client findByContactNumber(String number);
 }
