b4fea3612f8d0e69e1d337163054ce15f1c46fa2
Niatomi <playervoker@gmail.com>
2022-05-13 04:08:16 +0400

    Implemented save on event

--- rest-client-service/src/main/java/ru/niatomi/restClientService/event/listener/ClientListener.java
+++ rest-client-service/src/main/java/ru/niatomi/restClientService/event/listener/ClientListener.java
@@ -1,14 +1,25 @@
 package ru.niatomi.restClientService.event.listener;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.context.ApplicationListener;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Component;
 import ru.niatomi.restClientService.event.model.ClientSpringEvent;
+import ru.niatomi.restClientService.event.repository.EventRepository;
 
 /**
  * @author niatomi
  */
+@Component
+@AllArgsConstructor
 public class ClientListener implements ApplicationListener<ClientSpringEvent> {
+
+    private final EventRepository eventRepository;
+
     @Override
     public void onApplicationEvent(ClientSpringEvent clientSpringEvent) {
-
+        eventRepository.save(clientSpringEvent);
     }
+
 }
